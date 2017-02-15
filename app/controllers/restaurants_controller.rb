adcdde9a62128301518f72ee31c568f006fd05d2
Joy Joseph Abisaab <joy.abisaab@gmail.com>
2017-02-15 14:35:52 +0000

    complete all

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -26,23 +26,26 @@ end
 
 def update
   if @restaurant.update(restaurant_params)
-  redirect_to restaurant_path(@restaurant)
+    redirect_to restaurant_path(@restaurant)
+  else
+    render :edit
+  end
 end
 
-def destroy
-  @restaurant.destroy
-  redirect_to restaurants_path
-end
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
 
-private
+  private
 
-def set_restaurant
-  @restaurant = Restaurant.find(params[:id])
-end
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
-def restaurant_params
-  params.require(:restaurant).permit(:name, :address, :phone_number, :category)
-end
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
 
 
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,27 @@
 class ReviewsController < ApplicationController
  before_action :set_restaurant
 
-  def new
-    @review = Review.new
+ def new
+  @review = Review.new
+end
+
+def create
+  @review = Review.new(review_params)
+  @review.restaurant = @restaurant
+  if @review.save
+    redirect_to restaurant_path(@restaurant)
+  else
+    render :new
   end
+end
 
-  def create
-   @restaurant.reviews.create(review_params)
-   redirect_to restaurant_path(@restaurant)
-  end
+private
 
-  private
-
-  def set_restaurant
+def set_restaurant
   @restaurant = Restaurant.find(params[:restaurant_id])
-  end
+end
 
-  def review_params
-    params.require(:review).permit(:content,:rating)
-  end
+def review_params
+  params.require(:review).permit(:content,:rating)
+end
 end
